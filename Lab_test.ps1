66a36e0a955974b88a8d9f9690b095986bfdea19
johanlundberg92 <johan.lundberg1992@gmail.com>
2021-11-03 21:45:39 +0100

    fff

--- Lab_test.ps1
+++ Lab_test.ps1
@@ -1,14 +1,14 @@
 $labName = "JLAB"
+
+New-LabDefinition -Name $labName -DefaultVirtualizationEngine HyperV
 $PSDefaultParameterValues = @{
     'Add-LabMachineDefinition:Network' = $labName
-    'Add-LabMachineDefinition:ToolsPath'= "$labSources\Tools"
+    #'Add-LabMachineDefinition:ToolsPath'= "$labSources\Tools"
     'Add-LabMachineDefinition:DnsServer1'= '10.0.0.2'
     'Add-LabMachineDefinition:DomainName'= 'ACME.local'
     'Add-LabMachineDefinition:OperatingSystem'= 'Windows Server 2019 Standard (Desktop Experience)'
+    'Add-LabMachineDefinition:UserLocale'= 'en-SE'
 }
-
-
-New-LabDefinition -Name $labName -DefaultVirtualizationEngine HyperV
 
 # Network
 Add-LabVirtualNetworkDefinition -Name $labName -AddressSpace "10.0.0.0/24"
@@ -26,17 +26,20 @@ Add-LabMachineDefinition -Name DC01 -OperatingSystem "Windows Server 2019 Standa
 
 # == # Root CA
 $roles = Get-LabMachineRoleDefinition -Role CaRoot
-Add-LabMachineDefinition -Name ROOTCA01 -IpAddress 10.0.0.3 -Roles $roles
+Add-LabMachineDefinition -Name ROOTCA01 -IpAddress 10.0.0.3 -Roles $roles -IsDomainJoined
 
 # == # SubCA
 $roles = Get-LabMachineRoleDefinition -Role CaSubordinate
-Add-LabMachineDefinition -Name SUBCA01 -IpAddress 10.0.0.4 -Roles $roles
+Add-LabMachineDefinition -Name SUBCA01 -IpAddress 10.0.0.4 -Roles $roles -IsDomainJoined
 
 # == # Misc server
-Add-LabMachineDefinition -Name "SRV01" -IpAddress 10.0.0.5
+Add-LabMachineDefinition -Name "SRV01" -IpAddress 10.0.0.5 -IsDomainJoined
+
+#Install-Lab
 
 Install-Lab -BaseImages -NetworkSwitches -VMs
 Install-Lab -Domains
 Install-Lab -CA
+Install-Lab -StartRemainingMachines
 
 Show-LabDeploymentSummary -Detailed
